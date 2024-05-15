420159cafd52766ab4e18d65315c827b6e6dbc13
leonardo.coelho@uphold.com <leonardo.coelho@uphold.com>
2024-05-15 09:48:01 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NjIzNA/spectral.js?branch_slug=master&token=21105d62-19c5-4483-82a0-24de0d635c05";
+export default { extends: ruleset };
